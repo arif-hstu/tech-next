454a2ea034b845c8e8999dcf7dcf6d08f91cd675
arif-hstu <arif.dvm.hstu@gmail.com>
2021-07-01 01:02:25 +0600

    search bar issue fixed

--- src/components/Shared/Search/Search.js
+++ src/components/Shared/Search/Search.js
@@ -1,22 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { SearchContext } from '../../../App';
 
 import search from '../../../resources/icons/search.svg';
 import './Search.scss';
+
 const Search = () => {
     const [searchTerm, setSearchTerm] = useContext(SearchContext);
+    const [isHidden, setIsHidden] = useState(false);
 
     const searchItem = (event) => {
         event.preventDefault();
         setSearchTerm(event.target.value);
     };
 
+    // useEffect(() => {
+    //     if (searchTerm === "" && isHidden) {
+    //         document.getElementById('placeholder').style.display = "none";
+    //     } else if (searchTerm !== "" && isHidden) {
+    //         document.getElementById('placeholder').style.display = "block";
+    //     }
+    // }, [searchTerm, isHidden]);
+
     return (
         <div className="Search">
             <form className="searchHolder">
-                <input onChange={(event) => searchItem(event)} type="text" />
+                <input onBlur={() => setIsHidden(false)} onFocus={() => setIsHidden(true)} onChange={(event) => searchItem(event)} type="text" />
                 <img src={search} alt="Search" />
-                <p className="placeholder">
+                <p id="placeholder" className="placeholder">
                     Search in this page...
                 </p>
             </form>
